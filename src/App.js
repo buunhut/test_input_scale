1fcfe4495cccd6e9a4fd2d660f73920924f25f86
zero <zero@zeros-MacBook-Pro.local>
2023-10-07 13:09:22 +0700

    test input scale

--- src/App.js
+++ src/App.js
@@ -1,23 +1,310 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="header">
+        <div className="container">
+          <div className="content">
+            <div className="contentItem">
+              <i className="fa-solid fa-location-dot"></i>
+            </div>
+            <div className="contentItem">
+              <p>Bách hoá HÂN HÂN</p>
+            </div>
+            <div className="contentItem">
+              <i className="fa-solid fa-caret-down"></i>
+            </div>
+          </div>
+          <div className="content">
+            <div className="backItem">
+              <i className="fa-solid fa-angle-left"></i>
+            </div>
+            <div className="inputItem">
+              <input type="text" placeholder="Tìm kiếm sản phẩm,..." />
+              <i className="fa-solid fa-magnifying-glass myGlass"></i>
+              <i className="fa-solid fa-circle-xmark myClose"></i>
+
+            </div>
+            <div className="cart">
+              <i className="fa-solid fa-cart-shopping"></i>
+              <span>100</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="main">
+        <div className="container">
+          <div className="content">
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+            <div className="proItem">
+              <p>trang chủ</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="footer">
+        <div className="container">
+          <div className="content">
+            <div className="footerItem">
+              <i className="fa-solid fa-house"></i>
+              <p>Trang chủ</p>
+            </div>
+            <div className="footerItem">
+              <i className="fa-solid fa-layer-group"></i>
+              <p>Danh mục</p>
+            </div>
+            <div className="footerItem">
+              <i className="fa-solid fa-bag-shopping"></i>
+              <p>Thương hiệu</p>
+            </div>
+            <div className="footerItem">
+              <i className="fa-solid fa-bell"></i>
+              <p>Thông báo</p>
+            </div>
+            <div className="footerItem">
+              <i className="fa-solid fa-user"></i>
+              <p>Tài khoản</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
